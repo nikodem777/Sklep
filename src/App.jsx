89a64e397a11170d34546e8c1bad12b7b991b6cb
Nikodem <nikodem@MacBook-Air-Nikodem.local>
2025-11-03 11:04:40 +0100

    Pierwszy commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <a href="">Siema</a>
+    </div>
+  );
+}
+
+export default App;
